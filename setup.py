61e50fe6783c485687e976b478de2e2fa4afd50a
Yonatan Tarazona <ytarazona09@gmail.com>
2023-09-03 11:18:12 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""The setup script."""
+
+import io
+from os import path as op
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+here = op.abspath(op.dirname(__file__))
+
+# get the dependencies and installs
+with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
+    all_reqs = f.read().split("\n")
+
+install_requires = [x.strip() for x in all_reqs if "git+" not in x]
+dependency_links = [x.strip().replace("git+", "") for x in all_reqs if "git+" not in x]
+
+requirements = [ ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Yonatan Tarazona Coronel",
+    author_email='geoyons@gmail.com',
+    python_requires='>=3.5',
+    classifiers=[
+        #'Development Status ::  2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Remote Sensing Tools",
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    license="Apache Software License 2.0",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords='scikeo',
+    name='scikeo',
+    packages=find_packages(include=['scikeo', 'scikeo.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/ytarazona/scikit-eo',
+    version='0.2.16',
+    zip_safe=False,
+)
