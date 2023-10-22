6851ffa4e7ea260497e394a0cbdfe2cb89c0691f
Yonatan Tarazona <ytarazona09@gmail.com>
2023-10-21 19:50:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/yotarazona/scikit-eo',
-    version='0.2.2',
+    version='0.2.22',
     zip_safe=False,
 )
