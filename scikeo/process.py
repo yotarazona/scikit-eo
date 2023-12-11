81ee5decf654d542553de272696e8637b7853ac0
Yonatan Tarazona <ytarazona09@gmail.com>
2023-12-11 15:12:30 -0500

    Update process.py

--- scikeo/process.py
+++ scikeo/process.py
@@ -204,6 +204,7 @@ def confintervalML(matrix, image_pred, pixel_size = 10, conf = 1.96, nodata = No
             if nodata is None:
                 image_pred = image_pred.astype(float)
             else:
+                image_pred = image_pred.astype(float)
                 image_pred[image_pred == nodata] = np.nan
         
             # classes
