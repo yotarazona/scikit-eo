8b7ae0a3ba5cbac1ad0f8e7364ffdb11ab17620b
Yonatan Tarazona <ytarazona09@gmail.com>
2024-06-02 11:28:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10'
     ],
     description="Remote Sensing Tools",
     install_requires=install_requires,
