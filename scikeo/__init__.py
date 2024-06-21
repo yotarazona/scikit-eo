aa83f5594ae387c3bea1ff9863b08d3b64f6590e
Yonatan Tarazona <ytarazona09@gmail.com>
2024-06-21 16:20:52 -0500

    Update __init__.py

--- scikeo/__init__.py
+++ scikeo/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for scikit-eo."""
 
+import importlib.metadata
+
 __author__ = """Yonatan Tarazona Coronel"""
 __email__ = 'geoyons@gmail.com'
-__version__ = '0.1.0'
+__version__ = importlib.metadata.version('scikeo')
