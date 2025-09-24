6d8d45562dfedea3df7003767f3d1d5aa3ab4002
Yonatan Tarazona <ytarazona09@gmail.com>
2025-09-24 17:35:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,18 +26,16 @@ test_requirements = [ ]
 setup(
     author="Yonatan Tarazona",
     author_email='ytarazona09@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.8',
     classifiers=[
         #'Development Status ::  2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     description="Remote Sensing Tools",
     install_requires=install_requires,
